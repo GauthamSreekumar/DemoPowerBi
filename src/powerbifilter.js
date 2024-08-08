54ed5eb4099680086850bb0d91b2ee7c8b3f136d
GauthamSreekumar <gauthamsreekumar22@gmail.com>
2024-08-08 23:44:49 +0530

    filtered_using_acc_no

--- src/powerbifilter.js
+++ src/powerbifilter.js
@@ -1,33 +1,22 @@
-// PowerBIFilter.js
 import React, { useEffect } from "react";
 import { models } from "powerbi-client";
 
-const filterMappings = {
-  1: [
-    {
-      target: { table: "AR_AGING_VIEW", column: "ACCOUNT_NUM" },
-      operator: "In",
-      values: ["00011096"],
-    },
-  ],
-  4: [
-    {
-      target: { table: "AR_AGING_VIEW", column: "ACCOUNT_NUM" },
-      operator: "In",
-      values: ["00011271"],
-    },
-  ],
-};
-
 const PowerBIFilter = ({ inputValue, report }) => {
   useEffect(() => {
-    if (report) {
+    if (report && inputValue) {
       applyFilters(inputValue);
     }
   }, [inputValue, report]);
 
   const applyFilters = (input) => {
-    const filters = filterMappings[input] || [];
+    const filters = [
+      {
+        $schema: "http://powerbi.com/product/schema#basic",
+        target: { table: "AR_AGING_VIEW", column: "ACCOUNT_NUM" },
+        operator: "In",
+        values: [input],
+      },
+    ];
 
     report
       .updateFilters(models.FiltersOperations.Replace, filters)
